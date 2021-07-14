88d142f179023fcccc167ec1137f5da8a184848b
nishapathak13 <nish.pathak12@gmail.com>
2021-07-14 22:12:45 +0545

    responsive completed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 var burger = document.querySelector(".hamburger");
 var navItems = document.querySelector(".nav-items");
+var overlay = document.querySelector(".overlay");
+var bannerLg = document.querySelector('.mobile-image');
 
 burger.addEventListener('click', function(){
     navItems.classList.toggle('open');
     burger.classList.toggle('isactive');
+    overlay.classList.toggle('opacity');
+    document.body.classList.toggle('nav-open');
+    bannerLg.classList.toggle('opacity-1');
 })
